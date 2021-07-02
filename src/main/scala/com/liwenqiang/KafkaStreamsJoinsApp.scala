5b5f0775694f71d5a93b09db961dff012c31a7b3
liwenqiang <liwenqiang@pwrd.com>
2021-07-02 19:10:36 +0800

    add join example

--- src/main/scala/com/liwenqiang/KafkaStreamsJoinsApp.scala
+++ src/main/scala/com/liwenqiang/KafkaStreamsJoinsApp.scala
@@ -1,46 +1,73 @@
 package com.liwenqiang
 
-import com.liwenqiang.util.model.Purchase
+import com.liwenqiang.joiner.PurchaseJoiner
+import com.liwenqiang.util.model.{CorrelatedPurchase, Purchase}
 import com.liwenqiang.util.serde.StreamsSerdes
-import com.liwenqiang.util.serializer.{JsonDeserializer, JsonSerializer}
 
 import java.util.Properties
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.{KeyValue, StreamsConfig}
-import org.apache.kafka.streams.kstream.{KeyValueMapper, Predicate}
-import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.scala.kstream.Consumed
-import org.apache.kafka.streams.scala.serialization.Serdes.stringSerde
-import com.liwenqiang.util.serde.StreamsSerdes.{PurchasePatternSerde, PurchaseSerde}
+import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsBuilder, StreamsConfig, kstream}
+import org.apache.kafka.streams.kstream.{BranchedKStream, JoinWindows, KStream, KeyValueMapper, Named, Predicate, Printed}
+import org.apache.kafka.streams.scala.kstream.{Branched, Consumed, KTable, Produced, StreamJoined}
+
+import java.time.Duration
+import java.util
 
 
 object KafkaStreamsJoinsApp {
   def main(args: Array[String]): Unit = {
     val streamsConfig = new StreamsConfig(getProperties)
-    val builder = new StreamsBuilder()
+    val builder: StreamsBuilder = new StreamsBuilder()
     val purchaseSerde: Serde[Purchase] = StreamsSerdes.PurchaseSerde
     val stringSerde: Serde[String] = Serdes.String()
+    val consumed: Consumed[String, Purchase] = Consumed.`with`[String, Purchase](stringSerde, purchaseSerde)
+    val produced: Produced[String, Purchase] = Produced.`with`[String, Purchase](stringSerde, purchaseSerde)
+    val isCoffee: Predicate[String, Purchase] = (_: String, v: Purchase) => {
+      v.getDepartment.equalsIgnoreCase("coffee")
+    }
+    val isElectronics: Predicate[String, Purchase] = (_: String, v: Purchase) => {
+      v.getDepartment.equalsIgnoreCase("electronics")
+    }
+    val transactionStream: KStream[String, Purchase] = builder.stream("transactions", consumed)
+      .map((k: String, v: Purchase) => {
+        val masked: Purchase = Purchase.builder(v).maskCreditCard().build()
+        new KeyValue[String, Purchase](masked.getCustomerId, masked)
+      })
 
-    val custIdCCMasking:KeyValueMapper[String,Purchase,KeyValue[String,Purchase]] = (k:String,v:Purchase)=>{
-      val masked: Purchase = Purchase.builder(v).maskCreditCard().build()
-      new KeyValue[String,Purchase](masked.getCustomerId,masked)
-    }
-    val isCoffee:Predicate[String,Purchase] = {
-      (key:String,purchase:Purchase) => {
-        purchase.getDepartment.equalsIgnoreCase("coffee")
+    val branchStream: util.Map[String, KStream[String, Purchase]] = transactionStream.selectKey(new KeyValueMapper[String, Purchase, String] {
+      override def apply(key: String, value: Purchase): String = {
+        value.getCustomerId
       }
+    }).split(Named.as("foo-")) //添加别名
+      .branch(isCoffee, Branched.as("coffee"))
+      .branch(isElectronics, Branched.as("electronics"))
+      .noDefaultBranch()
+
+    val coffeeStream: kstream.KStream[String, Purchase] = branchStream.get("foo-coffee")
+    val electronicsStream: kstream.KStream[String, Purchase] = branchStream.get("foo-electronics")
+
+    val purchaseJoiner = new PurchaseJoiner
+
+    val twentyMinuteWindow: JoinWindows = JoinWindows.of(Duration.ofMinutes(20))
+
+    val joinedStream: kstream.KStream[String, CorrelatedPurchase] = coffeeStream.join(
+      electronicsStream
+      , purchaseJoiner
+      , twentyMinuteWindow,
+      StreamJoined.`with`[String, Purchase, Purchase](stringSerde, purchaseSerde, purchaseSerde)
+    )
+
+    joinedStream.print(Printed.toSysOut.asInstanceOf[Printed[String,CorrelatedPurchase]].withLabel("joined KStream"))
+
+    val kafkaStreams = new KafkaStreams(builder.build(), getProperties)
+
+    kafkaStreams.start()
+    sys.ShutdownHookThread {
+      kafkaStreams.close(Duration.ofSeconds(10))
     }
-    val isElectronics:Predicate[String,Purchase] = {
-      (key:String,purchase:Purchase) => {
-        purchase.getDepartment.equalsIgnoreCase("electronics")
-      }
-    }
-    import org.apache.kafka.streams.scala.ImplicitConversions._
-    builder.stream("transactions",Consumed.`with`[String,Purchase](stringSerde,purchaseSerde))(consumedFromSerde)
 
   }
-
 
 
   private def getProperties = {
@@ -56,7 +83,7 @@ object KafkaStreamsJoinsApp {
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1)
-//    props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, TransactionTimestampExtractor.get)
+    //    props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, TransactionTimestampExtractor.get)
     props
   }
 
